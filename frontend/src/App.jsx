6ea3b97e88743cad2d15dbf003363aa3514c2eef
lokeshadhi <lokeshadhi19@gmail.com>
2026-08-07 20:35:23 +0530

    path added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/browse" element={<BrowseProducts />} />
-        
+        <Route path="/products/:id" element={<ProductDetails />} />
 
         <Route
           path="/sell"
