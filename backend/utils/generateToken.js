6efb174560fda65e638e62a1cdc0aeaa25b881df
lokeshadhi <lokeshadhi19@gmail.com>
2026-09-11 04:20:20 +0530

    changed samesite of res.cookie

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,11 +9,12 @@ const generateToken = (res, userId) => {
   });
 
   res.cookie("token", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-  });
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  domain: process.env.COOKIE_DOMAIN || undefined,
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
   return token;
 };
